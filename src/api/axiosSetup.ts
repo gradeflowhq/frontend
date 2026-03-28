af5de46ae47f168e9fe2c05fb31a99b210f13f72
Muhammad Rizki Aulia Rahman Maulana <rizki@rizkiarm.com>
2026-03-28 23:02:38 +0800

    feat: specify API url via env variable

--- src/api/axiosSetup.ts
+++ src/api/axiosSetup.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosRequestHeaders, type InternalAxiosRequestConfig } from
 import { api } from './index';
 import { useAuthStore } from '@state/authStore';
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL ?? 'http://localhost:8000';
 
 let isRefreshing = false;
 let queue: Array<(token: string | null) => void> = [];
