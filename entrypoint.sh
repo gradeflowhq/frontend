21c224a93a166911aeb932a0bec8a3e966b9d669
Muhammad Rizki Aulia Rahman Maulana <rizki@rizkiarm.com>
2026-04-18 22:05:55 +0800

    fix: entrypoint should perform envsubst for ZITADEL_* variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Generate /config.js from the template using runtime environment variables.
-# Pass API_URL and CORS_PROXY_URL as Docker env vars.
-envsubst '${API_URL} ${CORS_PROXY_URL}' \
+# Pass API_URL, CORS_PROXY_URL, ZITADEL_AUTHORITY, ZITADEL_CLIENT_ID, and ZITADEL_ORG_DOMAIN as Docker env vars.
+envsubst '${API_URL} ${CORS_PROXY_URL} ${ZITADEL_AUTHORITY} ${ZITADEL_CLIENT_ID} ${ZITADEL_ORG_DOMAIN}' \
   < /etc/nginx/config.js.template \
   > /usr/share/nginx/html/config.js
 
